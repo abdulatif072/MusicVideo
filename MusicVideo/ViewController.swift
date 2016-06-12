419b86d3fa6eef7d027a108dcf459e4544b1b05d
Abdulatif Almulhim <abdulatifalmulhim@abdulatifs-macbook-pro-2.local>
2016-06-12 04:43:13 +0300

    part 7 to 10 completed

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -2,62 +2,83 @@
 //  ViewController.swift
 //  MusicVideo
 //
-//  Created by Abdulatif Almulhim on ١ رمضان، ١٤٣٧ هـ.
-//  Copyright © ١٤٣٧ Abdulatif Almulhim. All rights reserved.
+//  Created by Michael Rudowsky on 9/10/15.
+//  Copyright © 2015 Michael Rudowsky. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
- var videos = [Videos]()
+    
+    var videos = [Videos]()
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
+        
         
         //Call API
         let api = APIManager()
         api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+    }
+    
+    func didLoadData(videos: [Videos]) {
         
-
+        print(reachabilityStatus)
+        
+        self.videos = videos
+        
+        for item in videos {
+            print("name = \(item.vName)")
+        }
+        
+        for (index, item) in videos.enumerate() {
+            print("\(index) name = \(item.vName)")
+        }
         
         
+        //        for i in 0..<videos.count {
+        //            let video = videos[i]
+        //            print("\(i) name = \(video.vName)")
+        //        }
         
+        //        for var i = 0; i < videos.count; i++ {
+        //            let video = videos[i]
+        //            print("\(i) name = \(video.vName)")
+        //        }
         
     }
     
-    func didLoadData(videos:[Videos]){
+    func reachabilityStatusChanged()
+    {
         
-
-     
-        
-//        for var i = 0 ; i<videos.count;i+=1{
-//            let video = videos[i]                // another way in loop to display data
-//        print("name \(video.vName)")
-//        }
-        
-        
-        for item in videos {
-            
-            print(item.vName)
+        switch reachabilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+        displayLabel.text = "Reachable with Cellular"
+        default:return
         }
-    
-////        let alert = UIAlertController(title: (result), message: nil, preferredStyle: UIAlertControllerStyle.Alert)
-////        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-////        self.presentViewController(alert, animated: true, completion: nil)  // ok only
-//        
-//        let alert = UIAlertController(title: (result), message: nil, preferredStyle: UIAlertControllerStyle.Alert)
-//        
-//        // add the actions (buttons)
-//        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-//        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
-//        
-//        // show the alert
-//        self.presentViewController(alert, animated: true, completion: nil)
         
+    }
+    
+    // Is called just as the object is about to be deallocated
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+    
+    
+    
+    
+    
 }
-
-}
-   
-
-
-
 
